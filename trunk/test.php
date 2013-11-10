8dac5e63a3138371bf7d56c153dfd94e98babd0d
whatelsenes@gmail.com <whatelsenes@gmail.com@49049ab1-65af-b137-d98c-5c9246d35d27>
2013-11-10 16:34:12 +0000

    Premier commit

---
+++ trunk/test.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf8" />
+	<title>Projet Pâté</title>
+</head>
+<body>
+<h1>Projet Pâté</h1>
+<div>
+<?php
+	// on se connecte à MySQL
+	$db = mysql_connect('localhost', 'root', '');
+	mysql_set_charset('utf8');
+
+	// on sélectionne la base
+	mysql_select_db('boutiqueculturelle',$db);
+
+	// on crée la requête SQL
+	$sql = 'SELECT * FROM test';
+
+	// on envoie la requête
+	$req = mysql_query($sql) 
+		or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
+
+	// on fait une boucle qui va faire un tour pour chaque enregistrement
+	while($data = mysql_fetch_assoc($req)) {
+    	// on affiche les informations de l'enregistrement en cours
+		print_r($data);
+?>
+	<br />
+<?php
+    	}
+
+// on ferme la connexion à mysql
+mysql_close();
+?> 
+</div>
+</html>
