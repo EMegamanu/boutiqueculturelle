c3361758df2df1af9f45e3886155d65fe2edb7a1
whatelsenes@gmail.com <whatelsenes@gmail.com@49049ab1-65af-b137-d98c-5c9246d35d27>
2013-11-24 16:56:41 +0000

    amélioration menu

--- trunk/inc/header.inc.php
+++ trunk/inc/header.inc.php
@@ -20,11 +20,23 @@
 					<a href="disques.php">Disques</a>
 					<ul>
 						<li><a href="disques.php?cat=cd">CD</a></li>
-						<li><a href="disques.php?cat=demat">Dématérialisé</a></li>
+						<li><a href="disques.php?cat=demat">Dématérialisés</a></li>
 					</ul>
 				</li>
-				<li><a href="films.php">Films</a></li>
-				<li><a href="livres.php">Livres</a></li>
+				<li>
+					<a href="films.php">Films</a>
+					<ul>
+						<li><a href="films.php?cat=dvd">DVD</a></li>
+						<li><a href="films.php?cat=bluray">Blu-ray</a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="livres.php">Livres</a>
+					<ul>
+						<li><a href="livres.php?cat=relie">Reliés</a></li>
+						<li><a href="livres.php?cat=ebook">E-books</a></li>
+					</ul>
+				</li>
 				<li><a href="packs.php">Pack Culture</a></li>
 				<li><input type="search" name="recherche" placeholder="Tapez votre recherche"/></li>
 
