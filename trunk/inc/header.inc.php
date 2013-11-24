c172f0589f060d1743acc2f824cca5fc7f6a0a1f
whatelsenes@gmail.com <whatelsenes@gmail.com@49049ab1-65af-b137-d98c-5c9246d35d27>
2013-11-24 16:23:41 +0000

    test menu dynamique

--- trunk/inc/header.inc.php
+++ trunk/inc/header.inc.php
@@ -7,14 +7,27 @@
 		<link rel="stylesheet" href="css/style.css" />
 		<link rel="icon" href="img/logo-16.png" />
 	</head>
+
 	<body>
+
 	<header>
 		<img src="img/logo-64.png" alt="Boutique culturelle" id="logo"/>
 		<h1><?php echo $title; ?></h1>
+
 		<nav>
-			<ul>
-				<li><a href="disque.php">Disque</a></li>
-				<li><a href=""></a></li>
+			<ul id="menu-principal">
+				<li>
+					<a href="disques.php">Disques</a>
+					<ul>
+						<li><a href="disques.php?cat=cd">CD</a></li>
+						<li><a href="disques.php?cat=demat">Dématérialisé</a></li>
+					</ul>
+				</li>
+				<li><a href="films.php">Films</a></li>
+				<li><a href="livres.php">Livres</a></li>
+				<li><a href="packs.php">Pack Culture</a></li>
+				<li><input type="search" name="recherche" placeholder="Tapez votre recherche"/></li>
+
 			</ul>
 		</nav>
 
