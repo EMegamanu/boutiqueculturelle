d2e27f53f3f75511e197b89b3ac165d84dcdad70
whatelsenes@gmail.com <whatelsenes@gmail.com@49049ab1-65af-b137-d98c-5c9246d35d27>
2013-11-27 16:44:36 +0000

    Pied de page et début de css

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,2 +1,9 @@
+		<footer>
+			<ul>
+				<li><a href="contact.php">Contacts</a></li>
+				<li><a href="mentions.php">Mentions légales</a></li>
+				<li><a href="conditions.php">Conditions générales de ventes</a></li>
+			</ul>
+		</footer>
 	</body>
 </html>
