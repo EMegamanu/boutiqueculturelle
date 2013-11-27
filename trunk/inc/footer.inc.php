f91f4a8382950b27c57ff32f5491399ed5ce800c
whatelsenes@gmail.com <whatelsenes@gmail.com@49049ab1-65af-b137-d98c-5c9246d35d27>
2013-11-27 20:51:37 +0000

    Fusion avec style de Ruqiya

--- trunk/inc/footer.inc.php
+++ trunk/inc/footer.inc.php
@@ -1,9 +1,12 @@
 		<footer>
-			<ul>
-				<li><a href="contact.php">Contacts</a></li>
-				<li><a href="mentions.php">Mentions légales</a></li>
-				<li><a href="conditions.php">Conditions générales de ventes</a></li>
-			</ul>
+			<div id="information">	
+				<ul>
+					<li><a href="contact.php">Contacts</a></li>
+					<li><a href="mentions.php">Mentions légales</a></li>
+					<li><a href="conditions.php">Conditions générales de ventes</a></li>
+				</ul>
+			</div>
+		
 		</footer>
 	</body>
 </html>
