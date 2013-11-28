d9edd909cd26f25775c26f98698a693856211f16
whatelsenes@gmail.com <whatelsenes@gmail.com@49049ab1-65af-b137-d98c-5c9246d35d27>
2013-11-28 20:04:50 +0000

    Corrections mineurs de la pagination.

--- trunk/inc/header.inc.php
+++ trunk/inc/header.inc.php
@@ -40,16 +40,14 @@
 						 widgetOptions: {
 						 	filter_hideFilters : true,
 						 	filter_reset: '#menu2 a.reset',
-					     	filter_filteredRow: 'filtered',
+					     	filter_filteredRow: 'filtered'
 						 }
 					})
-					.tablesorterPager({container: $(this).next(".pager")});
+					.tablesorterPager({
+						container: $(this).next(".pager"),
+						output: '{page} / {totalPages}'
+					});
 				});
-
-				  // External search
-  // buttons set up like this:
-  // <button type="button" data-filter-column="4" data-filter-text="2?%">Saved Search</button>
-
 
 			  $("#menu2 a:not(.reset)").click(function(evt) {
 			  	var $lien = $(this);
@@ -63,24 +61,6 @@
 
 			  	evt.preventDefault();
 			  });
-
-
-  $('button[data-filter-column]').click(function(){
-    /*** first method *** data-filter-column="1" data-filter-text="!son"
-      add search value to Discount column (zero based index) input */
- var filters = [],
-      $t = $(this),
-      col = $t.data('filter-column'), // zero-based index
-      txt = $t.data('filter-text') || $t.text(); // text to add to filter
-
-    filters[col] = txt;
-    // using "table.hasFilters" here to make sure we aren't targetting a sticky header
-    $.tablesorter.setFilters( $('table.hasFilters'), filters, true ); // new v2.9
-
-    
-
-    return false;
-  });
 			});
 		</script>
 	</head>
