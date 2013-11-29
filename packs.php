ed5490c371c9903a5564890cbe01588da027e368
ruqiyakhalid@gmail.com <ruqiyakhalid@gmail.com@49049ab1-65af-b137-d98c-5c9246d35d27>
2013-11-29 16:16:56 +0000

    ajout page pack

---
+++ packs.php
@@ -0,0 +1,20 @@
+<?php
+include_once('inc/header.inc.php');
+?>
+
+<body>
+  <br> <br>
+       <h1 align ="center"> Pack culture</h1>
+	   
+	   <p align="center"> Bienvenue dans votre pack.</p>
+<p align="center"> Cet espace vous permettra de cr&#233er votre pack &#233tape par &#233tape en toute simplicit&#233
+     </p>
+	 <p align="center"> Vous devrez dans un premier temps choisir un CD,puis un DVD et enfin un Livre </p>
+	 <P align="center"> L'avantage du pack est de vous faire beneficier d'une reduction importante </p>
+	 </body>
+  
+  <form method="get"action="lapremiereetape.php">
+  <p align ="center"><input type="submit" value="Cr&#233er votre pack -> " rows=6 COLS=26 style="font-family:arial" style="border style:solid" style="background:#9900ff" style="color:#ff66ff"></form> </p>
+  
+  <br>
+  <br>
