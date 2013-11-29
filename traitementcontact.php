20ed764fd4f9fe542cd67e5f4d6b7c0a33fb0a89
ruqiyakhalid@gmail.com <ruqiyakhalid@gmail.com@49049ab1-65af-b137-d98c-5c9246d35d27>
2013-11-29 21:15:54 +0000

    phpmailer

---
+++ traitementcontact.php
@@ -0,0 +1,24 @@
+<?php
+ 
+   // On va chercher la définition de la classe
+   require('class.phpmailer.php');
+        $mail = new PHPmailer(); 
+        $mail->IsSMTP(); 
+        $mail->Host="smtp.gmail.com"; 
+        $mail->From='courriel'; 
+        $mail->AddAddress('ruqiyakhalid@gmail.com'); 
+        $mail->AddReplyTo('courriel');      
+        $mail->Subject='boutique culturelle'; 
+        $mail->Body='Voici un exemple d\'e-mail au format Texte'; 
+        if(!$mail->Send()){ //Teste le return code de la fonction 
+          echo $mail->ErrorInfo; //Affiche le message d'erreur (ATTENTION:voir section 7) 
+        } 
+        else{      
+          echo 'Mail envoyé avec succès'; 
+        } 
+        $mail->SmtpClose(); 
+        unset($mail); 
+?> 
+
+ 
+?>
