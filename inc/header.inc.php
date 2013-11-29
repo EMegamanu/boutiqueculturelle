b0720591e31294f63dd6e63ad11198b08c72ccce
whatelsenes@gmail.com <whatelsenes@gmail.com@49049ab1-65af-b137-d98c-5c9246d35d27>
2013-11-29 23:18:09 +0000

    Correction noms des pages

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -82,17 +82,17 @@
 		</h1>
 		<nav id="espace_client">
 			<ul>
-				<li><a href="inscription.html">Inscription</a></li>
-				<li><a href="Connexion">Espace client</a></li>
+				<li><a href="inscription.html" tabindex="61">Inscription</a></li>
+				<li><a href="Connexion" tabindex="71">Espace client</a></li>
 			</ul>
 		</nav>
 	</header>
 		
 	<nav id="menu-principal">
 		<ul>
-			<li><a href="pageCD.php" tabindex="1">Disques</a></li>
-			<li><a href="DVD.php" tabindex="11">Films</a></li>
-			<li><a href="livre.php" tabindex="21">Livres</a></li>
+			<li><a href="disques.php" tabindex="1">Disques</a></li>
+			<li><a href="films.php" tabindex="11">Films</a></li>
+			<li><a href="livres.php" tabindex="21">Livres</a></li>
 			<li><a href="packs.php" tabindex="31">Pack Culture</a></li>
 			<li><a href="admin.php" tabindex="41">Administration</a></li>
 			<li class="search-item"><input type="search" name="recherche" placeholder="Tapez votre recherche" tabindex="51"/></li>
