b117012f995a5bfe9a64762ccda15c38066ca53a
whatelsenes@gmail.com <whatelsenes@gmail.com@49049ab1-65af-b137-d98c-5c9246d35d27>
2013-11-29 20:32:31 +0000

    Amélioration page accueil, taille dynamique

--- trunk/index.php
+++ trunk/index.php
@@ -7,22 +7,24 @@
 	/* Inclusion de l'en-tête. */
 	include_once('inc/header.inc.php');
 ?>
-<section>
+<section id="accueil">
+	<div class="group">
+		<aside id="pub">
+			<a href="packs.php"><img src="img/pub.png" alt="Composez votre pack culture"/></a>
+		</aside>
+
+		<article id="bienvenue">
+			<p>Bienvenue sur la boutique culturelle en ligne.</p>
+
+			<p>Nous proposons de nombreux <strong>Livres</strong>, <strong>Disques</strong> et <strong>Films</strong>. Notre catalogue de produits se compose ainsi de classiques mais aussi de nouveautés.</p>
+
+			<p>Nous vous proposons également de composer un <strong>Pack Culture</strong> économique composé d'un Disque, d'un Film et d'un Livre pour seulement 29,99 euros.</p>
+
+			<p>Notre équipe reste à votre disposition en cas de question ou de problème de navigation.</p>
+
+		</article>
+	</div>
 	
-	<article>
-		<p>Bienvenue sur la boutique culturelle en ligne.</p>
-
-		<p>Nous proposons de nombreux <strong>Livres</strong>, <strong>Disques</strong> et <strong>Films</strong>. Notre catalogue de produits se compose ainsi de classiques mais aussi de nouveautés.</p>
-
-		<p>Nous vous proposons également de composer un <strong>Pack Culture</strong> économique composé d'un Disque, d'un Film et d'un Livre pour seulement 29,99 euros.</p>
-
-		<p>Notre équipe reste à votre disposition en cas de question ou de problème de navigation.</p>
-
-	</article>
-	
-	<aside>
-		<a href="packs.php"><img src="img/pub.png" alt="offre_promotionnelle" id="pub"/></a>
-	</aside>
 </section>
 
 
