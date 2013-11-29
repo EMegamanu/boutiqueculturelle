4f343a7c7dc8021e18b9bc18d54c56bcd95d4ba9
boggioaudrey@gmail.com <boggioaudrey@gmail.com@49049ab1-65af-b137-d98c-5c9246d35d27>
2013-11-29 16:51:21 +0000

    modifs pages

--- latroisiemeetape.php
+++ latroisiemeetape.php
@@ -7,20 +7,21 @@ include_once('inc/header.inc.php');;
 	   <p align="center"> Vous voila dans la deuxi&#232me etape de votre PACK.</p>
 <p align="center">	   Nous vous demanderons de choisir dans la liste ci apr&#232s le Livre que vous souhaitez mettre dans votre pack
      </p>
-	 </body>
-  
 
+</body>
+  <br>
+  <br>
   
   <div id="menu2">
 	<ul>
-		<li><a href="#">Policier</a></li>
-		<li><a href="#">Amour</a></li>
-		<li><a href="#">BD/Manga</a></li>
-		<li><a href="#">Aventure</a></li>
-		<li><a href="#">Essais</a></li>
+		<li><a href="#">Tout</a></li>
+		<li><a href="#">Manga</a></li>
+		<li><a href="#">BD</a></li>
+		<li><a href="#">Romans</a></li>
 	</ul>
 </div>
 
 <form method="get"action="recapitulatif.php">
   
   <p align ="center"><input type="submit" value="Terminer -> " rows=6 COLS=26 style="font-family:arial" style="border style:solid" style="background:#9900ff" style="color:#ff66ff"></form> </p>
+  
