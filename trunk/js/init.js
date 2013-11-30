8d9300f3363583655d87d632973cd7121bad5cbf
whatelsenes@gmail.com <whatelsenes@gmail.com@49049ab1-65af-b137-d98c-5c9246d35d27>
2013-11-30 16:22:27 +0000

    Correction formulaire inscription

--- trunk/js/init.js
+++ trunk/js/init.js
@@ -1,41 +1,49 @@
 /* Au chargement de la page achevé... */
 $(function() {
+	/* Indice de la colonne du genre dans le tableau de résultats de la page courante. */
 	var indiceColGenre = -1;
 
   	$(".results th").each(function(i) {
   		if($(this).text() == "Genre") {
   			indiceColGenre = i;
-  			return false;
+  			return false; /* Trouvé ! On sort de la boucle. */
   		}
   	});
 
 	/* Initialisation de jQuery.tablesorter pour tous les tableaux de résultat. */
 	$("table.results").each(function() {
 		var $table = $(this);
-		$table.tablesorter({
+		$table.tablesorter({ /* Trie du tableau. */
 			widgets: ["zebra", "filter"],
 			 widgetOptions: {
-			 	filter_hideFilters : true,
+			 	filter_hideFilters : true, /* + 1 règle dans le CSS pour totalement masquer les champs. */
 			 	filter_reset: '#menu2 a.reset',
 		     	filter_filteredRow: 'filtered'
 			 }
 		})
-		.tablesorterPager({
+		.tablesorterPager({ /* Pagination. */
 			container: $(this).next(".pager"),
 			output: '{page} / {filteredPages}'
 		});
 	});
 
-  $("#menu2 a:not(.reset)").click(function(evt) {
-  	var $lien = $(this);
 
-    var filters = [];
-    filters[indiceColGenre] = $lien.text();
+	/* Initialisation de l'application des filtres via le menu latéral. */
+	$("#menu2 a:not(.reset)").click(function(evt) {
+	  	var $lien = $(this);
 
-   
-    $.tablesorter.setFilters( $('table.results'), filters, true ); // new v2.9
+	    var filters = [];
+	    filters[indiceColGenre] = $lien.text();
+
+	    $.tablesorter.setFilters( $('table.results'), filters, true );
 
 
-  	evt.preventDefault();
-  });
+	  	evt.preventDefault();
+	});
+
+	/* Fallback gestion des champs de type Date sur navigateurs ne les gérant pas encore. (Ex : Firefox) */
+	$inputDate = $("input[type='date']");
+	if($inputDate.prop("type") == "text") {
+		$inputDate.datepicker();
+	}
 });
