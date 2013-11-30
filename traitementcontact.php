3f5028e89b9dd38c0feceeb25431fb542d847997
ruqiyakhalid@gmail.com <ruqiyakhalid@gmail.com@49049ab1-65af-b137-d98c-5c9246d35d27>
2013-11-30 10:32:38 +0000

    modif page

--- traitementcontact.php
+++ traitementcontact.php
@@ -1,26 +1,29 @@
 <?php
-
   include_once('inc/header.inc.php');
- 
+define('GUSER', 'boutiqueculturellentic'); // GMail username
+define('GPWD', 'boutique2013'); // GMail password 
    // On va chercher la définition de la classe
-   require('class.phpmailer.php');
-        $mail = new PHPmailer(); 
-        $mail->IsSMTP(); 
-        $mail->Host="smtp.gmail.com"; 
-        $mail->From='courriel'; 
-        $mail->AddAddress('ruqiyakhalid@gmail.com'); 
-        $mail->AddReplyTo('courriel');      
-        $mail->Subject='boutique culturelle'; 
-        $mail->Body='Voici un exemple d\'e-mail au format Texte'; 
-        if(!$mail->Send()){ //Teste le return code de la fonction 
-          echo $mail->ErrorInfo; //Affiche le message d'erreur (ATTENTION:voir section 7) 
-        } 
-        else{      
-          echo 'Mail envoyé avec succès'; 
-        } 
-        $mail->SmtpClose(); 
-        unset($mail); 
-?> 
-
- 
-?>
+   require_once('inc/PHPMailer/class.phpmailer.php');
+        $mail = new PHPmailer();
+        $mail->IsSMTP(); // enable SMTP
+        $mail->SMTPDebug = 0;  // debugging: 1 = errors and messages, 2 = messages only 
+        $mail->SMTPAuth = true;  // authentication enabled
+        $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
+        $mail->Host = 'smtp.gmail.com';
+        $mail->Port = 465; 
+        $mail->Username = boutiqueculturellentic;  
+        $mail->Password = boutique2013;      
+        $mail->SetFrom='From: '$_POST['courriel'];
+        $mail->Subject = "contact boutique culturelle";
+        $mail->Body= "Nom : ".$_POST['nom']."\r\n";"Prénom : ".$_POST['prenom']."\r\n"
+                    "Adresse email : ".$_POST['courriel']."\r\n";"Téléphone : ".$_POST['telephone']."\r\n".
+                    "N° Commande : ".$_POST['nun_commande']."\r\n".
+                    "Message : ".$_POST['message']."\r\n".
+        $mail->AddAddress($_POST['courriel']);
+        if(!$mail->Send()) {
+    $error = 'Erreur: '.$mail->ErrorInfo; 
+    return false;
+  } else {
+    $error = 'Message envoyé!';
+    return true;
+  }
