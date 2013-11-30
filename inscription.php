030ba0ed671272525ba1403fccff9ff06474791a
whatelsenes@gmail.com <whatelsenes@gmail.com@49049ab1-65af-b137-d98c-5c9246d35d27>
2013-11-30 16:22:27 +0000

    Correction formulaire inscription

--- inscription.php
+++ inscription.php
@@ -1,39 +1,65 @@
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-	
-
-	<meta charset="utf-8"/>
-       
-        <title>Inscription</title>
 <?php		
-/* Inclusion script connexion base de données. */
+	$title = 'Inscription';
+	
+	/* Inclusion script connexion base de données. */
 	require_once('inc/db.inc.php');
 	/* Inclusion de l'en-tête. */
 	include_once('inc/header.inc.php');
 ?>
-	
-	<br/> <br/>
-</div>
-			<h2>Inscription</h2>
 
-<form action="nouveau_client.php" "method="post">
-	<div><label>Nom*:&nbsp;&nbsp;</label><input type="text" placeholder="Ex. DUPONT"size=20 maxlength=30 name="nom"/></div>
-	<div> <label>Pr&#233nom*:&nbsp;&nbsp;<label/><input type="text" placeholder="Ex. Nicolas"size=20 maxlength=30 name="prenom"/></div>
-	<div><label>Identifiant*:&nbsp;&nbsp;</label><input type="text" size=20 maxlength=30 name="idUtilisateur"/></div>
-<div><label>Adresse*:&nbsp;&nbsp;</label><input type="text" maxlength=50 name="Adresse"/></div>
-<div><label>Ville*:&nbsp;&nbsp;</label/><input type="text"size=20 maxlength=30 name="Ville"/></div>
-<div><label>CP*:&nbsp;&nbsp;</label><input type="text"size=10 maxlength=5 name="CP"/></div>
+	<section>
+		<form action="nouveau_client.php" method="post">
+			<div>
+				<label for="nom">Nom</label><abbr title="requis">*</abbr> :
+				<input type="text" placeholder="Ex. DUPONT" size=20 maxlength=30 name="nom" id="nom" required="required" />
+			</div>
 
-<div><label>Adresse email*:&nbsp;&nbsp;</label><input type="email" placeholder="Ex. dupont@gmail.com"size=20 maxlength=40 name="email"/></div>
-<div><label>Mot de passe*:&nbsp;&nbsp;</label><input type="password"size=20 maxlength=20 name="motdepasse"/></div>
-<div><label>Confirmer mot de passe*:&nbsp;&nbsp;</label><input type="password""size=20 maxlength=20 name="motdepasse2"/></div>
-<div>
- <input type="submit" value="Envoyer" />
- <input type="reset" value="Annuler" />
- </div>
-<br/></form>
-<br/> <br/><br/> <br/><br/><br/> <br/><br/> <br/><br/> 
+			<div>
+				<label for="prenom">Prénom</label><abbr title="requis">*</abbr> :
+				<input type="text" placeholder="Ex. Nicolas" size=20 maxlength=30 name="prenom" id="prenom" required="required" />
+			</div>
+
+			<div>
+				<label for="idUtilisateur">Identifiant</label><abbr title="requis">*</abbr> :
+				<input type="text" size=20 maxlength=30 name="idUtilisateur" id="idUtilisateur" required="required" />
+			</div>
+
+			<div>
+				<label for="adresse">Adresse</label><abbr title="requis">*</abbr> :
+				<input type="text" maxlength=50 name="Adresse" id="adresse" required="required" />
+			</div>
+
+			<div>
+				<label for="ville">Ville</label><abbr title="requis">*</abbr> :
+				<input type="text" size=20 maxlength=30 name="ville" id="ville" required="required" />
+			</div>
+			
+			<div>
+				<label for="cp"><abbr title="Code postal">CP</abbr></label><abbr title="requis">*</abbr> :
+				<input type="text" size=10 maxlength=5 name="cp" id="cp" required="required" />
+			</div>
+
+			<div>
+				<label for="email">Adresse email</label><abbr title="requis">*</abbr> :
+				<input type="email" placeholder="Ex. dupont@gmail.com" size=20 maxlength=40 name="email" id="email" required="required" />
+			</div>
+
+			<div>
+				<label for="mot_de_passe">Mot de passe</label><abbr title="requis">*</abbr> :
+				<input type="password" size=20 maxlength=20 name="mot_de_passe" id="mot_de_passe" required="required" />
+			</div>
+			
+			<div>
+				<label for="mot_de_passe2">Confirmer</label><abbr title="requis">*</abbr> :
+				<input type="password" size=20 maxlength=20 name="mot_de_passe2" id="mot_de_passe2" required="required" />
+			</div>
+
+			<div>
+				<input type="submit" value="Envoyer" />
+				<input type="reset" value="Annuler" />
+			</div>
+		</form>
+	</section>
 <?php
     /* Inclusion du pied de page. */
     include_once('inc/footer.inc.php');
