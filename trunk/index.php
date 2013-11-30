c051f4a95ac24a326fc3eefa28dd37039dcb42a6
whatelsenes@gmail.com <whatelsenes@gmail.com@49049ab1-65af-b137-d98c-5c9246d35d27>
2013-11-30 20:42:05 +0000

    modif page index

--- trunk/index.php
+++ trunk/index.php
@@ -10,15 +10,15 @@
 <section id="accueil">
 	<div class="group">
 		<aside id="pub">
-			<a href="packs.php"><img src="img/pub2.png" alt="Composez votre pack culture"/></a>
+			<a href="packs.php"><img src="img/pub.png" alt="Composez votre pack culture"/></a>
 		</aside>
 
 		<article id="bienvenue">
 			<p>Bienvenue sur la boutique culturelle en ligne.</p>
 
-			<p>Nous vous proposons de nombreux <strong>Livres</strong>, <strong>Disques</strong> et <strong>Films</strong>. Notre catalogue de produits se compose ainsi de classiques mais aussi de nouveautés.</p>
+			<p>Nous vous proposons de nombreux <strong>Livres</strong>, <strong>Disques</strong> et <strong>Films</strong>. Notre catalogue de produits comprend aussi bien des classiques que les dernières nouveautés.</p>
 
-			<p>Disponible seulement sur notre site internet: la possibilité de composer un <strong>Pack Culture</strong> économique comprenant un Disque, un Film et un Livre pour seulement 29,99 euros.</p>
+			<p>Disponible seulement sur notre site internet: la possibilité de composer un <strong>Pack Culture</strong> économique comprenant un Disque, un Film et un Livre. Vous obtiendrez ainsi une réduction de 10 % sur le montant de votre Pack.</p>
 
 			<p>Notre équipe reste à votre disposition en cas de question ou de problème de navigation.</p>
 
@@ -27,11 +27,23 @@
 	</div>
 </section>
 
-<section>
-	<h2>Nouveautés du mois</h2>
-</section>
-		
+<section id="nouveautes">
+	<h2><span class="title">Nouveautés du mois</span></h2>
 
+	<?php
+		$results = $db->query('SELECT * FROM Produit ORDER BY id DESC LIMIT 0, 5');
+		  while($data = $results->fetch()) {
+?>
+		<figure>
+		<a href="fiche_produit.php?p=<?php echo $data['id']?>"><img src="<?php echo $data['image'];?>" alt="" /></a>
+		<figcaption><?php echo $data['nom'].'<br />';?></figcaption>
+		</figure>
+	
+<?php
+		  }
+	?>
+
+</section>
 
 <?php
 	/* Inclusion du pied de page. */
