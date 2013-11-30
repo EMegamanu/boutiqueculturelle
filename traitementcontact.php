bd8aa1114d9562b072fad1d76b3598ac648a7d4e
ruqiyakhalid@gmail.com <ruqiyakhalid@gmail.com@49049ab1-65af-b137-d98c-5c9246d35d27>
2013-11-30 11:34:54 +0000

    motif traitementcontact.php

--- traitementcontact.php
+++ traitementcontact.php
@@ -4,6 +4,7 @@ define('GUSER', 'boutiqueculturellentic'); // GMail username
 define('GPWD', 'boutique2013'); // GMail password 
    // On va chercher la définition de la classe
    require_once('inc/PHPMailer/class.phpmailer.php');
+function smtpmailer( & ($_POST['nom']), & ($_POST['prenom']), &($_POST['courriel']) & ($_POST['telephone']), & ($_POST['n°commande']), & ($_POST['message'])) { 
         $mail = new PHPmailer();
         $mail->IsSMTP(); // enable SMTP
         $mail->SMTPDebug = 0;  // debugging: 1 = errors and messages, 2 = messages only 
@@ -13,12 +14,12 @@ define('GPWD', 'boutique2013'); // GMail password
         $mail->Port = 465; 
         $mail->Username = boutiqueculturellentic;  
         $mail->Password = boutique2013;      
-        $mail->SetFrom='From: '$_POST['courriel'];
+        $mail->SetFrom='From: '.$_POST['courriel'].;
         $mail->Subject = "contact boutique culturelle";
         $mail->Body= "Nom : ".$_POST['nom']."\r\n";"Prénom : ".$_POST['prenom']."\r\n"
                     "Adresse email : ".$_POST['courriel']."\r\n";"Téléphone : ".$_POST['telephone']."\r\n".
                     "N° Commande : ".$_POST['nun_commande']."\r\n".
-                    "Message : ".$_POST['message']."\r\n".
+                    "Message : ".$_POST['message']."\r\n".;
         $mail->AddAddress($_POST['courriel']);
         if(!$mail->Send()) {
     $error = 'Erreur: '.$mail->ErrorInfo; 
@@ -27,3 +28,4 @@ define('GPWD', 'boutique2013'); // GMail password
     $error = 'Message envoyé!';
     return true;
   }
+}
