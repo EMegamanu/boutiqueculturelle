9ff4191a767987723ca2f95f9812f92417e721fa
whatelsenes@gmail.com <whatelsenes@gmail.com@49049ab1-65af-b137-d98c-5c9246d35d27>
2013-11-30 16:00:46 +0000

    Déplacement script d'initialisation dans fichier externe

--- trunk/inc/header.inc.php
+++ trunk/inc/header.inc.php
@@ -22,49 +22,7 @@
 		<link rel="stylesheet" href="css/jquery.tablesorter/theme.default.css" />
 		<link rel="stylesheet" href="css/jquery.tablesorter/jquery.tablesorter.pager.css" />
 
-		<script>
-			/* Au chargement de la page achevé... */
-			$(function() {
-				var indiceColGenre = -1;
-
-			  	$(".results th").each(function(i) {
-			  		if($(this).text() == "Genre") {
-			  			indiceColGenre = i;
-			  			return false;
-			  		}
-			  	});
-
-				/* Initialisation de jQuery.tablesorter pour tous les tableaux de résultat. */
-				$("table.results").each(function() {
-					var $table = $(this);
-					$table.tablesorter({
-						widgets: ["zebra", "filter"],
-						 widgetOptions: {
-						 	filter_hideFilters : true,
-						 	filter_reset: '#menu2 a.reset',
-					     	filter_filteredRow: 'filtered'
-						 }
-					})
-					.tablesorterPager({
-						container: $(this).next(".pager"),
-						output: '{page} / {filteredPages}'
-					});
-				});
-
-			  $("#menu2 a:not(.reset)").click(function(evt) {
-			  	var $lien = $(this);
-
-			    var filters = [];
-			    filters[indiceColGenre] = $lien.text();
-
-			   
-			    $.tablesorter.setFilters( $('table.results'), filters, true ); // new v2.9
-
-
-			  	evt.preventDefault();
-			  });
-			});
-		</script>
+		<script src="js/init.js"></script>
 	</head>
 
 	<body>
