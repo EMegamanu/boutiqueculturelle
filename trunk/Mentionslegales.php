4ab58fcd82966a9f6821fcc497113dd59e94a3f5
whatelsenes@gmail.com <whatelsenes@gmail.com@49049ab1-65af-b137-d98c-5c9246d35d27>
2013-12-01 18:31:05 +0000

    améliorations index, mentions légales

--- trunk/Mentionslegales.php
+++ trunk/Mentionslegales.php
@@ -4,46 +4,32 @@
 	include_once('inc/header.inc.php');
 
 ?>
- <head>
-        <meta charset="utf-8" />
-        <link rel="stylesheet" href="css/style.css" />
-        <title>Mentions légales</title>
-    </head>
 
-<Center><H1>Mention légales</H1></center>
 <Section class= "mentionslegales">
-<H2>Informations légales</h2>
-<p>
-<ol>
-	<li>Le présent site est édité par: Audrey Boggio, Nicolas Dos Santos, Nadia Elidrissi, Ruqiya Khalid et Charlotte Moreau étudiants au capital social de 0 euros </Li>
-	<li>Pour toute demande de contact vous pouvez nous contacter à l’adresse suivante boutiqueculturellentic@gmail.com.</Li>
- </ol>
- </p>
-<H2>Condition d'utilisations</H2>
-<p>
-<ol> 
-	<li>Ce cite étant créé dans le cadre d’un projet de cours, en aucun cas les produits proposés ne sont disponibles à la vente.</li>
-	<Li>Nous nous réservons le droits de modifier tout contenu jusqu’à la rendu du dît projet.</li>
-	<Li>Nous ne serons pas tenus responsables par la mauvaise utilisation que vous pourrez faire du dît site</li>
-</ol>
-</p>
-<H2>Données relatives aux clients</H2>
-<P>
-<ol>
-	<li>Les données saisies par le client sont confidentielles et ne peuvent être communiqué à un tiers.</li>
-	<Li>Tout message Client/Fournisseurs ne peut être divulgué sans l’accord des deux parties.</li>
-</ol>
-</p>
+	<h2>Informations légales</h2>
+		<ol>
+			<li>Le présent site est édité par: Audrey Boggio, Nicolas Dos Santos, Nadia Elidrissi, Ruqiya Khalid et Charlotte Moreau étudiants au capital social de 0 euro. </Li>
+			<li>Pour toute demande de contact vous pouvez nous contacter à l’adresse suivante boutiqueculturellentic@gmail.com.</Li>
+		 </ol>
+	<h2>Condition d'utilisations</h2>
+		<ol> 
+			<li>Ce site étant créé dans le cadre d’un projet de cours, en aucun cas les produits proposés ne sont disponibles à la vente.</li>
+			<Li>Nous nous réservons le droit de modifier tout contenu jusqu’à la rendu dudit projet.</li>
+			<Li>Nous ne serons pas tenus responsables par la mauvaise utilisation que vous pourrez faire dudit site</li>
+		</ol>
+	<h2>Données relatives aux clients</h2>
+		<ol>
+			<li>Les données saisies par le client sont confidentielles et ne peuvent être communiquées à un tiers.</li>
+			<Li>Tout message Client/Fournisseurs ne peut être divulgué sans l’accord des deux parties.</li>
+		</ol>
 
-<H2>Loi Applicable</H2>
-<p>
-<ol>
-	<li>Loi communautaire et  loi Française.</li>
-</ol>
-</p>
+	<h2>Lois Applicables</H2>
+		<ol>
+			<li>Loi communautaire et la Loi Française.</li>
+		</ol>
 
-</section>
-<?php
+	</section>
+	<?php
 	/* Inclusion du pied de page */
 	include_once('inc/footer.inc.php');
 ?>
