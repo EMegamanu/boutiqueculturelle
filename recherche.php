ee9ca1946adf031a8173250ba9c8b72c52c9fed5
whatelsenes@gmail.com <whatelsenes@gmail.com@49049ab1-65af-b137-d98c-5c9246d35d27>
2013-12-01 15:38:09 +0000

    ajout recherche

--- recherche.php
+++ recherche.php
@@ -1,11 +1,13 @@
-<pre>
+
 <?php 
 	/* Inclusion script connexion base de données. */
 	require_once('inc/db.inc.php');
+	/* Inclusion de l'en-tête. */
+	include_once('inc/header.inc.php');
 
 $requete = 
 'SELECT * FROM ( ' .
-	'SELECT C.nom categorie, P.id id, P.nom nom, P.prixHT prixHT, D.genre genre, D.compositeur artiste, D.anneeProduction annee ' .
+	'SELECT C.nom categorie, P.id id, P.nom nom, P.prixHT prixHT, D.genre genre, D.compositeur artiste, D.anneeProduction annee, P.image image ' .
 	'FROM Produit P ' .
 	'JOIN Disque D ON P.id = D.id ' .
 	'JOIN Categorie C ' . 
@@ -15,7 +17,7 @@ $requete =
 	'OR D.genre LIKE CONCAT( :recherche, \'%\') ' .
 	'OR D.anneeProduction = :recherche ' .
 	'UNION ' .
-	'SELECT C.nom categorie, P.id id, P.nom nom, P.prixHT prixHT, F.genre genre, F.realisateur artiste, F.anneeProduction annee ' .
+	'SELECT C.nom categorie, P.id id, P.nom nom, P.prixHT prixHT, F.genre genre, F.realisateur artiste, F.anneeProduction annee, P.image image ' .
 	'FROM Produit P ' .
 	'JOIN Film F ' .
 	'ON P.id = F.id ' .
@@ -26,7 +28,7 @@ $requete =
 	'OR F.genre LIKE CONCAT( :recherche, \'%\') ' .
 	'OR F.anneeProduction = :recherche ' .
 	'UNION ' .
-	'SELECT C.nom categorie, P.id id, P.nom, P.prixHT prixHT, L.genre genre, L.auteur artiste, L.dateParution annee ' .
+	'SELECT C.nom categorie, P.id id, P.nom, P.prixHT prixHT, L.genre genre, L.auteur artiste, L.dateParution annee, P.image image ' .
 	'FROM Produit P ' .
 	'JOIN Livre L ' .
 	'ON P.id = L.id ' .
@@ -45,11 +47,39 @@ $results = $db->prepare($requete);
 
 $results->bindValue(':recherche', $_GET['recherche']);
 $results->execute();
-$results->setFetchMode(PDO::FETCH_OBJ);
+// $results->setFetchMode(PDO::FETCH_OBJ);
 	// print_r($results);
-
+?>
+<table class='results'>
+	<thead>
+		<tr>
+			<th>Pochette</th>
+			<th>Nom</th>
+			<th>Genre</th>
+			<th>Artiste</th>
+			<th>Année</th>
+			<th>Prix</th>
+			<th>Catégorie</th>
+		</tr>
+	</thead>
+	<tbody>
+<?php		
 while($produit = $results->fetch()) {
-	print_r($produit);
+?>
+<tr>
+	<td><img src ="<?php echo $produit['image'];?>" alt="" </></td>
+	<td><?php echo $produit['nom'];?></td>
+	<td><?php echo $produit['genre'];?></td>
+	<td><?php echo $produit['artiste'];?></td>
+	<td><?php echo $produit['annee'];?></td>
+	<td><?php echo $produit['prixHT'];?></td>
+	<td><?php echo $produit['categorie'];?></td>
+</tr>
+<?php
 }
 ?>
-</pre>
+</tbody>
+</table>
+<!-- categorie, id, nom, prixHT,  genre,  artiste,  annee ' . -->
+
+
