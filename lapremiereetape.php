668aa3bd50d9e2bcf56ffc588f4d73c080d339d7
whatelsenes@gmail.com <whatelsenes@gmail.com@49049ab1-65af-b137-d98c-5c9246d35d27>
2013-12-02 06:41:06 +0000

    divers

--- lapremiereetape.php
+++ lapremiereetape.php
@@ -1,21 +1,12 @@
 <?php
 include_once('inc/header.inc.php');
 ?>
- <body>
-  <br> <br>
-       <h1 align="center"> Etape 1: Choisir un CD </h1>
-	   
-	   <p align="center"> Vous voila dans la premi&#232re etape de votre PACK.</p>
-<p align="center">	   Nous vous demanderons de choisir dans la liste ci apr&#232s le CD que vous souhaitez mettre dans votre pack
-     </p>
-	 </body>
 
-	 
-	 
+    <h1> Etape 1: Choisir un CD </h1>
+	   <p > Vous voila dans la premi&#232re etape de votre PACK.</p>
+    <p>	Nous vous demanderons de choisir dans la liste ci-après le CD que vous souhaitez mettre dans votre pack.</p>	 
   <form method="get"action="ladeuxiemeetape.php">
-  <p align="center"> <input type="submit" value="Passez &#224 l'etape 2 -> " rows=6 COLS=26 style="font-family:arial" style="border style:solid" style="background:#9900ff" style="color:#ff66ff"> </form> </p>
-  
-  
+  <p> <input type="submit" value="Passez &#224 l'etape 2 -> " rows=6 COLS=26 style="font-family:arial" style="border style:solid" style="background:#9900ff" style="color:#ff66ff"> </form> </p>
   	 
 <div id="menu2">
 	<ul>
